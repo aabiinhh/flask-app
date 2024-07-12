e92fd7030c8110bc108c5e225afd6a2c76e4880d
abin y <abiny432@gmail.com>
2024-07-12 16:30:43 +0530

    final commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+from database import get_data
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return render_template('index.html', )
+
+@app.route('/about')
+def about():
+    data = get_data()
+    return render_template('about.html',data=data)
+
+@app.route('/newpage')
+def newpage():
+    return render_template('newpage.html')
+
+[]
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__=="main":
+    app.run()
